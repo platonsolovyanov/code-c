029da6e75adc97f2d756bad03041244145b84644
sheshin <sheshin@MacBook-Air-sheshin.local>
2022-10-17 16:37:19 +0200

    remember commit

---  hello.c
+++  hello.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <math.h> 
 
-main()
-{
-    float kilometer, mile;
-    float lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    kilometer = lower;
-
-    while (kilometer <= upper)
-    {
-        mile = kilometer * 0.621;
-
-        printf("%3.2f :  %6.2f\n", kilometer, mile);
-        kilometer = kilometer + step;
+int main () {
+    int x;
+    scanf("%d", &x);
+    printf("   |");
+    for (int i = 0; i < x; i++){
+        int cislo = x-i;  
+        printf("  %d  ", cislo);        
+        for (int yoop = 0; yoop < cislo; yoop++){
+            // printf("%d \n", cislo * yoop+1);
+            // printf("%d \n", yoop+1);
+            printf("_");
+        }
     }
 }
